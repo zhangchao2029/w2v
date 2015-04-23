51723e58b9916264d1cd0fb5252436cc730b1e50
zhangchao2029 <zhangchao5656@gmail.com>
2015-04-23 15:30:56 +0800

    Revert "Revert "Revert "w2v"""
    This reverts commit d5e9b890e02b361f5fdf4db61a00fe05781184c4.

--- getCategoryName.m
+++
@@ -1,5 +0,0 @@
-label_name='flykite_clear';
-[x1]= textread(['/home/SA14011105/matlab/w2v_imageNet_1500/labels_category/',label_name,'.txt'],'%s','delimiter',' ');
-x1=x1'
-eval([label_name,'_image_names=x1;']);
-save(['labels/',label_name,'.mat'],[label_name,'_image_names']);
